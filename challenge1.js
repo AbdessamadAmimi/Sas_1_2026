f97c9d1dbc21516d155d5f773fe04c42de645673
AbdessamadAmimi <amimiabdo936@gmail.com>
2026-09-01 11:11:01 +0100

    challenge 1

---
+++ challenge1.js
@@ -0,0 +1,12 @@
+let prompt = require("prompt-sync")();
+let Pseudo = prompt("Entrer Pseudo: ");
+let Niveau = prompt("Entrer Niveau: ");
+let Âge = prompt("Entrer Âge: ");
+let Score = prompt("Entrer Score: ");
+let Pays = prompt("Entrer Pays: ");
+console.log("---------------------------------------");
+console.log("Pseudo: " ,Pseudo)
+console.log("Niveau: " ,Niveau)
+console.log("Âge: " ,Âge)
+console.log("Score: " ,Score)
+console.log("Pays: " ,Pays)
