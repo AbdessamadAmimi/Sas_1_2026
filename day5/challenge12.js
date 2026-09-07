26f239e38dfe4eef8d974d2d31d76d4630dc007b
AbdessamadAmimi <amimiabdo936@gmail.com>
2026-09-07 11:54:55 +0100

    elever au carre et modifier challenge 6

---
+++ day5/challenge12.js
@@ -0,0 +1,8 @@
+function eleverAuCarre(tab){
+    return tab.map((e) => {
+        return e * e
+    })
+    
+}
+
+console.log(eleverAuCarre([2,4,6]))
--- day5/challenge6.js
+++ day5/challenge6.js
@@ -1,7 +1,7 @@
 function inverserTableau(tab){
     let rev_tab = [];
     for(let i=tab.length-1 ; i>=0 ; i--){
-        rev_tab[i] = tab[tab.length-1-i]
+        rev_tab[tab.length-1-i] = tab[i]
     }
     return rev_tab;
 }
